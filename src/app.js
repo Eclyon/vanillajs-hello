b757d9e051021a079a758ef424c3f6460ef26ec3
Agustin <73564466+Eclyon@users.noreply.github.com>
2024-07-23 06:27:49 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var who = ["Mi Perro", "Mi Abuela", "El Cartero", "Un Pajaro"];
+  var action = ["se comio", "rayo", "destruyo", "corto"];
+  var what = ["mi cuaderno", "mi celular", "mi cartera", "mi pc"];
+  var when = [
+    "despues de clases",
+    "mientras estaba durmiendo",
+    "cuando estaba distraido",
+    "mientras estaba comiendo"
+  ];
+
+  function generateExcuse() {
+    var randomWho = who[Math.floor(Math.random() * who.length)];
+    var randomAction = action[Math.floor(Math.random() * action.length)];
+    var randomWhat = what[Math.floor(Math.random() * what.length)];
+    var randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  document
+    .getElementById("generate-button")
+    .addEventListener("click", generateExcuse);
+  generateExcuse();
 };
